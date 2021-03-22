29358f745d861e56ec84bda434227f45d3ee1788
ayane93sese <koro.115ayapi@gmail.com>
2021-03-22 12:41:45 +0900

    バリデーション記述

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,2 +1,10 @@
 class Task < ApplicationRecord
+
+  validates :title, presence:true
+  validate :date_before_start
+
+end
+
+def date_before_start
+  errors.add(:start_time, "は過去の日付を選択できません") if start_time < Date.today
 end
